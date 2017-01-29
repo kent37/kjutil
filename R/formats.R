5a8a07710971287cd9ae642519376361b2df061f
Kent Johnson <kent3737@gmail.com>
2017-01-29 14:27:43 -0500

    Support user CSS

--- R/formats.R
+++ R/formats.R
@@ -5,11 +5,17 @@
 #' @return R Markdown output format to pass to \code{\link{render}}
 
 #' @export
-small_format <- function(...) {
+small_format <- function(css=NULL, ...) {
 
-  # get the locations of resource files located within the package
-  css <- system.file('rmarkdown', 'templates', 'small_format', 'resources', 'small_format.css', package = "kjutil")
+  # get the location of our custom CSS
+  css_all <- system.file('rmarkdown', 'templates', 'small_format', 'resources',
+                         'small_format.css',
+                         package = "kjutil")
+
+  # Add any user-provided CSS
+  if (!is.null(css))
+    css_all = c(css_all, css)
 
   # call the base html_vignette function
-  rmarkdown::html_vignette(css = css, ...)
+  rmarkdown::html_vignette(css = css_all, ...)
 }
